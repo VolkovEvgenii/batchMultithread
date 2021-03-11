5f02f536f70e4750d54b64d81fd1c3abd44ee7d4
evgeniy volkov <volkov.e.n@gmail.com>
2021-03-11 16:41:54 +0300

    init commit

---
+++ src/test/java/ru/volkov/batch/multithread/MultithreadApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.volkov.batch.multithread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultithreadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
